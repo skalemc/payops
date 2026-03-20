b3a1ab62ad26850e2dc5858eec8eaf22e4a1fe6f
skalemc <skalemc.co@gmail.com>
2026-03-20 11:21:08 +0800

    Update db.js

--- server/lib/db.js
+++ server/lib/db.js
@@ -5,7 +5,7 @@ const { Pool } = pg;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: true }
+    ? { rejectUnauthorized: false }
     : false,
   max:              20,
   idleTimeoutMillis: 30_000,
